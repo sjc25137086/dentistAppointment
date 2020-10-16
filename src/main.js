7122c0f2f8cbf9ef1434a4ada7f970b200dfe610
sjc25137086 <891109354@qq.com>
2020-10-16 21:33:04 +0800

    xiao 10.16
    over vue

--- src/main.js
+++ src/main.js
@@ -6,9 +6,14 @@ import axios from 'axios'
 import qs from 'qs'
 import moment from 'moment';
 import Mint from 'mint-ui';
+<<<<<<< HEAD
+import '../public/css/reset.css'
+import 'mint-ui/lib/style.min.css';
+=======
 import 'mint-ui/lib/style.css'; 
 import '../public/css/reset.css'
 import 'mint-ui/lib/style.min.css'; 
+>>>>>>> 8ec6b1eb745d35af7e96c5a14524664088927923
 import { Popup } from 'mint-ui';
 
 Vue.component(Popup.name, Popup)
